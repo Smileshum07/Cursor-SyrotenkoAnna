5f9c28a8ffaf99c40f59128285d051b67f6c2131
Anna <103360175+Smileshum07@users.noreply.github.com>
2022-08-04 01:40:48 +0200

    15

--- HW №15(14)/js/main.js
+++ HW №15(14)/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
+function preloader() {
+    $(() => {
+
+        setInterval(() => {
+            let p = $('.preloader');
+
+        p.css('opacity', 0); 
+
+        }, 1000)
+       
+    })
+}
+
+preloader();
+
 console.log('test');
+
+$(document).ready(function () {
+    $('.review-slider').slick();
+});
